2f44195a7a85746e67efee5e3f4e3126d76695b6
Hadley, Caleb <caleb@hadleynet.org>
2025-11-09 10:37:31 -0500

    lint

--- iris_fisher/modeling/train.py
+++ iris_fisher/modeling/train.py
@@ -3,31 +3,40 @@ import pickle
 
 from loguru import logger
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_validate
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import cross_val_score, train_test_split, cross_validate
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 import typer
 
 from iris_fisher.config import MODELS_DIR, PROCESSED_DATA_DIR
 
 app = typer.Typer()
 
+
 def train_linear_model(X, y, model, model_path):
     pipeline = make_pipeline(StandardScaler(), model)
-    results = cross_validate(pipeline, X, y, cv=5, scoring=['accuracy', 'f1_macro'], return_estimator=False)
+    results = cross_validate(
+        pipeline, X, y, cv=5, scoring=["accuracy", "f1_macro"], return_estimator=False
+    )
     logger.info(f"Cross Validation Results {results}")
-    logger.info("%0.2f accuracy with a standard deviation of %0.2f" % (results['test_accuracy'].mean(), results['test_accuracy'].std()))
-    logger.info("%0.2f macro f1 with a standard deviation of %0.2f" % (results['test_f1_macro'].mean(), results['test_f1_macro'].std()))
+    logger.info(
+        "%0.2f accuracy with a standard deviation of %0.2f"
+        % (results["test_accuracy"].mean(), results["test_accuracy"].std())
+    )
+    logger.info(
+        "%0.2f macro f1 with a standard deviation of %0.2f"
+        % (results["test_f1_macro"].mean(), results["test_f1_macro"].std())
+    )
 
     # Retrain model on entire dataset
-    pipeline.fit(X, y) 
+    pipeline.fit(X, y)
 
     with open(model_path, "wb") as f:
         pickle.dump(pipeline, f)
@@ -54,25 +63,32 @@ def train_svm(X, y, model_path):
 def train_dec_tree(X, y, model_path):
     logger.info("Training DecisionTreeClassifier...")
     train_linear_model(X, y, DecisionTreeClassifier(), model_path)
-    logger.info(f"DecisionTreeClassifier Training Complete. Pickle can be found at: {model_path}\n")
+    logger.info(
+        f"DecisionTreeClassifier Training Complete. Pickle can be found at: {model_path}\n"
+    )
 
 
 def train_rfc(X, y, model_path):
     logger.info("Training RandomForestClassifier...")
     train_linear_model(X, y, RandomForestClassifier(), model_path)
-    logger.info(f"RandomForestClassifier Training Complete. Pickle can be found at: {model_path}\n")
+    logger.info(
+        f"RandomForestClassifier Training Complete. Pickle can be found at: {model_path}\n"
+    )
 
 
 def train_gbc(X, y, model_path):
     logger.info("Training GradientBoostingClassifier...")
     train_linear_model(X, y, GradientBoostingClassifier(), model_path)
-    logger.info(f"GradientBoostingClassifier Training Complete. Pickle can be found at: {model_path}\n")
+    logger.info(
+        f"GradientBoostingClassifier Training Complete. Pickle can be found at: {model_path}\n"
+    )
 
 
 def train_mlp(X, y, model_path):
     logger.info("Training MLPClassifier...")
     train_linear_model(X, y, MLPClassifier(), model_path)
     logger.info(f"MLPClassifier Training Complete. Pickle can be found at: {model_path}\n")
+
 
 def train_log_reg(X, y, model_path):
     logger.info("Training Logistic Regression...")
